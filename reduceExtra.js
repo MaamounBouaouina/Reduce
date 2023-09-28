408147fc00816ce305a5df0dfe89c65b6dde62e5
Tarek-Issaoui <124356474+Tarek-Issaoui@users.noreply.github.com>
2023-09-28 14:54:23 +0100

    Create reduceExtra.js

---
+++ reduceExtra.js
@@ -0,0 +1,63 @@
+// create a function that return the sum of the elements and each element multiplyed by his index using the .reduce() method.
+// example: sumOfElementsMultiplyedByIndex([1,2,3,4,5]) => 1*0 + 2*1 + 3*2 + 4*3 + 5*4 = 30
+
+
+
+
+
+
+
+
+
+
+
+//create a functtion tha takes twos arrays as a parameters and returned an array without the elements founded in the first array . using reduce() method.
+// example: removeElements([1,2,3,4,5], [1,2,3]) => [4,5]
+
+
+
+
+
+
+
+
+
+// create a function that take an array as a parameter with a duplicated value and return the number of duplication of every value inside that array using .reduce();
+//example: numberOfDuplication([1,2,3,4,5,1,2,3,4,5]) => {1:2,2:2,3:2,4:2,5:2}
+
+
+
+
+
+
+
+// create a function that take an array of arrays as a parameter and return a single array without the arrays inside that array.using .reduce() method.
+// example: flatten([[1,2,3,4,5],[1,2,3]]) => [1,2,3,4,5]
+
+
+
+
+
+
+
+
+
+
+
+//create  a function thet takes an array of object , figure out how much it would cost to just buy everything at once .
+//example:
+// var wishlist = [
+//     { title: "Tesla Model S", price: 90000 },
+//     { title: "4 carat diamond ring", price: 45000 },
+//     { title: "Fancy hacky Sack", price: 5 },
+//     { title: "Gold fidgit spinner", price: 2000 },
+//     { title: "A second Tesla Model S", price: 90000 }
+// ];
+//totalCost(wishlist) => 227005
+
+
+
+
+
+
+
